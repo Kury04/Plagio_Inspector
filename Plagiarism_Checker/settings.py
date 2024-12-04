95df83216e9d3d944a6932207e82fa632251d2d0
Kury04 <al222111497@gmail.com>
2024-12-04 15:08:40 -0600

    Actualización Host 2

--- Plagiarism_Checker/settings.py
+++ Plagiarism_Checker/settings.py
@@ -49,7 +49,7 @@ DEBUG = True
 
 #ALLOWED_HOSTS = ["127.0.0.1:8000"]
 
-ALLOWED_HOSTS = [ 'plagiarismcheckerpyresearch.herokuapp.com','localhost', '127.0.0.1']
+ALLOWED_HOSTS = [ 'plagiarismcheckerpyresearch.herokuapp.com','localhost', '127.0.0.1', '*']
 
 # Application definition
 
